e7a7a3ef8dc44b97d5423863066337df358767f8
Tâmara Thais <t.thais@escolar.ifrn.edu.br>
2025-02-06 17:14:26 +0000

    a

--- AGENDAMENTO/atv.sql
+++ AGENDAMENTO/atv.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Pessoa (
     Cpf CHAR(11) PRIMARY KEY,
     Nome VARCHAR (100) NOT NULL,
-    Email VARCHAR(100) UNIQUE NOT NUÇÇ,
+    Email VARCHAR(100) UNIQUE NOT NULL,
     Data_Nascimento DATE NOT NULL,
     Endereço VARCHAR(255),
     Telefone VARCHAR(20)
@@ -22,4 +22,9 @@ CREATE TABLE Medico (
 CREATE TABLE Especialidade (
     Identificador SERIAL PRIMARY KEY,
     Descricao VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE medico_especialidade (
+    Crm CHAR(6) NOT NULL REFERENCES Medico(Crm)
+    Identificador INT NOT NULL Identificador Medico (Cmr)
 );
